e5e08658c6be51e47c06267479ca974c88ae5efc
Ryan Toronto <ryanto@gmail.com>
2019-12-06 12:13:40 -0500

    Add cypress

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
+import { Server } from "miragejs";
 
-makeServer();
+if (window.Cypress) {
+  // mirage cypress server
+  let cyServer = new Server({
+    routes() {
+      ["get", "put", "patch", "post", "delete"].forEach(method => {
+        this[method]("/*", (schema, request) => {
+          return window.handleFromCypress(request);
+        });
+      });
+    }
+  });
+  cyServer.logging = false;
+} else {
+  // mirage dev server
+  makeServer();
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
